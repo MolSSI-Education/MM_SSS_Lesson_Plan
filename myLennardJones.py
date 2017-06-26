1aaaa1432fb80477172119c151870bdaf79280fe
Eliseo <emarinri@nd.edu>
2017-06-26 13:58:17 -0400

    Modify driver file to automatically convert to real untis

--- myLennardJones.py
+++ myLennardJones.py
@@ -5,17 +5,30 @@ from forceFieldManager import forceFieldManager
 from boxManager import boxManager
 import numpy as np
 
-myBox = box(length = 37.3)
-myBoxManager = boxManager(myBox)
-#myBoxManager.addParticles(n=200,method="lattice") 
-myBoxManager.getConfigFromFile(restartFile = "lj_sample_config_periodic1.txt")
+reducedTemperature = 0.85
+reducedDensity = 0.003
+numParticles = 500
 
-myForceField = lennardJones(parms = (3.73, 148.0), cutoff = 11.19)
+sigma = 3.73
+epsilon = 148.0
+
+realTemperature = epsilon*reducedTemperature
+
+boxLength = reducedDensity / np.power(sigma,3)
+boxLength = np.power(numParticles / boxLength,0.333)
+
+
+myBox = box(length = boxLength)
+myBoxManager = boxManager(myBox)
+myBoxManager.addParticles(n=numParticles,method="lattice") 
+#myBoxManager.getConfigFromFile(restartFile = "nistConfig.xyz")
+
+myForceField = lennardJones(parms = (sigma, epsilon), cutoff = 3*sigma)
 myForceFieldManager = forceFieldManager(myForceField)
 myForceFieldManager.assignSystemForceField(myBox)
 
-mySimulation = simulation(method = "monteCarlo", temperature = 133.2, 
-        steps = 1000000, printProp = 1000, printXYZ = 1000, maxDisp = 0.1,
+mySimulation = simulation(method = "monteCarlo", temperature = realTemperature, 
+        steps = 1000000, printProp = 1000, printXYZ = 1000, maxDisp = sigma,
         ffManager = myForceFieldManager, boxManager = myBoxManager)
 
 mySimulation.run()
