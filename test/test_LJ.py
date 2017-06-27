74376cdc91bf7313fd7fe0fb002cc4b8427e3bc2
Eliseo <emarinri@nd.edu>
2017-06-26 23:20:49 -0400

    Add test to compute single state NIST energy

--- test/test_LJ.py
+++ test/test_LJ.py
@@ -4,22 +4,19 @@ import pytest
 import mm_python as mmpy
 
 
-def test_lj_ff1():
+def test_lj():
 
     params = (2.5, 6.5)
-    cutoff = 10
+    cutoff = 10.0
 
-    lj_ff = mmpy.ForceField.lennardJones(params, cutoff) 
+    lj_ff = mmpy.LennardJones(params, cutoff) 
 
     rij2 = np.linspace(0.1, 50, 1000)
     
-    bench_energy = (np.power(params[0], 2) / rij2) ** 6 
+    bench_energy =  (np.power(params[0], 2) / rij2) ** 6 
     bench_energy -= (np.power(params[0], 2) / rij2) ** 3
     bench_energy *= 4.0 * params[1]
 
     energy = lj_ff.evaluate(rij2)
 
-    print(bench_energy[500])
-    print(energy[500])
-    print(np.linalg.norm(energy - bench_energy))
     assert np.allclose(energy, bench_energy)
---
+++ test/test_nistEnergy.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pytest
+import mm_python as mmpy
+
+
+def test_nistEnergy():
+
+
+    answer = (-4351.5,-198.49)
+
+    params = (3.73, 148.0)
+    rCut = 3*params[0]
+
+    myBox = mmpy.Box(length=10.0*params[0])
+    myBoxManager = mmpy.BoxManager(myBox)
+    myBoxManager.getConfigFromFile(restartFile = "test/nistConfig.xyz")
+
+    myForceField = mmpy.LennardJones(parms = params, cutoff = rCut)
+    ffManager = mmpy.ForceFieldManager(myForceField)
+    ffManager.assignSystemForceField(myBox)
+
+    pairEnergy = ffManager.getPairEnergy(myBox)
+    correctionEnergy = myForceField.getTailCorrection(myBox)
+
+    bench_energy = (pairEnergy/params[1], correctionEnergy/params[1])
+    assert np.allclose(answer, bench_energy)
+
