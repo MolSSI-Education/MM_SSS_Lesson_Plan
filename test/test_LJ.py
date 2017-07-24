4f029b1ba91ee378b19ed308a2a66f6661936a0e
John Chodera <john.chodera@choderalab.org>
2017-07-23 21:17:52 -0400

    Updated switch test

--- test/test_LJ.py
+++ test/test_LJ.py
@@ -8,13 +8,13 @@ def test_lj():
 
     cutoff = 10.0
 
-    lj_ff = mmpy.LennardJones(cutoff) 
+    lj_ff = mmpy.LennardJones(cutoff)
 
     rij2 = np.linspace(0.1, 50, 1000)
-    
-    bench_energy =  (1.0 / rij2) ** 6 
+
+    bench_energy =  (1.0 / rij2) ** 6
     bench_energy -= (1.0 / rij2) ** 3
-    bench_energy *= 4.0 
+    bench_energy *= 4.0
 
     energy = lj_ff.evaluate(rij2)
 
@@ -29,7 +29,7 @@ def test_switch():
     switch = 9.0
     cutoff = 10.0
 
-    lj_ff = mmpy.LennardJones(params, cutoff, switch=switch)
+    lj_ff = mmpy.LennardJones(cutoff, switch=switch)
 
     assert lj_ff.computeSwitch(switch**2) == 1.0
     assert lj_ff.computeSwitch(cutoff**2) == 0.0
